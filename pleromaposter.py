045bbcc29a8c46c0c93c29dfee7934a0fc6b9fca
Dan Radunchev <dan@govorenefekt.bg>
2022-12-30 18:12:47 +0200

    some shit

--- pleromaposter.py
+++ pleromaposter.py
@@ -19,16 +19,27 @@ headers["Content-Type"] = "application/json"
 
 
 def getstats():
-  r = requests.get(configParser['ge']['apiendpoint'])
-  return r.json()
+  data = requests.get(configParser['ge']['apiendpoint']).json()
+  stats = {
+    "feeds": data["stats"]["feedCountTotal"],
+    "totalep": data["stats"]["episodeCountTotal"],
+    "timestamp": data["as-of"],
+    "last3days": data["stats"]["NewEpisodes3days"],
+    "last10days": data["stats"]["NewEpisodes10days"],
+    "last30days": data["stats"]["NewEpisodes30days"],
+    "last90days": data["stats"]["NewEpisodes90days"]
+  }
+  return stats
 
 def main():
   data = getstats()
-  feeds = data["stats"]["feedCountTotal"]
-  totalep = data["stats"]["episodeCountTotal"]
-  timestamp = data["as-of"]
-
-  payload = {"status":  timestamp + "\n" + str(feeds) + " подкаста са публикували " + str(totalep) + " епизода на български език.\nhttps://podcastalot.com\n#podcasts #stat"}
+  payload = {"status":  data['timestamp'] + "\n" + str(data['feeds']) + \
+  " подкаста са публикували " + str(data['totalep']) + " епизода на български език.\n" + \
+  "За последните три дена: " + str(data['last3days']) + " епизода.\n" + \
+  "За последните десет дена: " + str(data['last10days']) + " епизода.\n" + \
+  "За последните тридесет дена: " + str(data['last30days']) + " епизода.\n" + \
+  "За последните деветдесет дена: " + str(data['last90days']) + \
+  " епизода.\nhttps://podcastalot.com\n#podcasts #stat"}
   response = requests.post(url, json=payload, headers=headers)
   return response.status_code
 
